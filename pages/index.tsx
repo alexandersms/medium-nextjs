77a7513c73d8ce34445df713bb97ff6a521fbcf8
Alex Simisi Nta <smsxander@gmail.com>
2022-04-07 01:59:27 +0200

    adding banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Banner from '../components/banner'
 import Header from '../components/header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium 3.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Header */}
       <Header />
+
+      {/* Banner */}
+      <Banner />
       
     </div>
   )
